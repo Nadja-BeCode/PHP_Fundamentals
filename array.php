700f8f0cd32ce34e732b1c602c013b81a5fae64d
Nadja-BeCode <nadja.agostinho@gmail.com>
2021-03-17 15:07:49 +0100

    update

--- array.php
+++ array.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
 //Create an array representing your family members
 //Display its content using the print_r function
 //Then, create an array describing your favourite recipes
@@ -17,4 +27,54 @@ echo $series[3]
 
 $series = ['Chicago Fire','Manifest','Queen of the South','Younger','Magnolias','Virgin River','Blacklist', 'Chasepeak Shore']
 echo var_dump($series);
+array_push($series, 'There for you');
+
+
+$countries[] = 'England';
+var_dump($countries);
+
+
+$person['function'] = 'Junior Web Developer';
+$person['function'] = 'Senior Web Developer';
+echo $person['function'];
+
+//<p>Coding an Associative Array</p>
+
+$user = array (
+    'firstname' => 'Juan',
+    'lastname' => 'Pablo',
+    'adress' => '3 Paradijsestraat',
+    'city' => 'Antwerpen'
+    );
+    var_dump($user);
+    echo $user['lastname'];
+
+//<p>Describe yourself using an associative array: $me Specify your age, your favourite season of the year, wether you like soccer or not (boolean). Try to use the right variable type for each value.</p>
+
+
+$me = array (
+    'age' => '7',
+    'favorite_season' => 'Winter',
+    'soccer' => false,
+    
+    );
+
+$me = array(
+    'age' => '7',
+    'favorite_season' => 'Winter',
+    'soccer' => false,
+    'favourite_movies' 	=> array('My Own Private Idaho', 'Her', 'Matrix')
+    );
+    
+    echo '<pre>';
+    print_r($me);
+    echo '</pre>';
+
+
+
+
+
 ?>
+
+</body>
+</html>
